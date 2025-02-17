72511d7f2ea2f3d8d626198334973489e41e04ee
EE533-TEAM3 <saint.augustine666@gmail.com>
2025-02-17 15:19:10 -0800

    Adding the generic_regs included ILA top
    Added logic for getting the data OUT from the hw towards the software

--- LAB5/ILA_Generic_Tested/trigger_generator.v
+++ LAB5/ILA_Generic_Tested/trigger_generator.v
@@ -7,22 +7,26 @@ module trigger_generator
     output reg trigger
   );
   
-  reg primedz, primedzz;
+  reg primedz, primedzz, primedzzz, primedzzzz;
   
-  always @(posedge clock)
-    begin
+   always @(posedge clock)
+   begin
       if(reset)
-        begin
-          primedz  <= 0;
-          primedzz <= 0;
-          trigger  <= 0;
-        end
+      begin
+         primedz     <= 0;
+         primedzz    <= 0;
+         primedzzz   <= 0;
+         primedzzzz  <= 0;
+         trigger     <= 0;
+      end
       else
-        begin
-          primedz  <= primed;
-          primedzz <= primedz;
-          trigger  <= primedz & ~primedzz;
-        end
-    end
+      begin
+         primedz   <= primed;
+         primedzz  <= primedz;
+         primedzzz <= primedzz;
+         primedzzzz<= primedzzz;
+         trigger   <= primedz & ~primedzz;
+      end
+   end
  
 endmodule
